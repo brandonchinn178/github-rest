fcaa7c3eefaa4d7c1ccbe99fb4a8c12911be5f0d
Brandon Chinn <brandon@leapyear.io>
2019-09-26 17:54:26 -0700

    Add endpointPath tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,8 @@
 import Test.Tasty (defaultMain, testGroup)
 
+import qualified Endpoint
+
 main :: IO ()
 main = defaultMain $ testGroup "github-rest"
-  [
+  [ testGroup "GitHub.REST.Endpoint" Endpoint.tests
   ]
