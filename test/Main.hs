ee9eef87ec606a78b7682f99e8ae3b63cf5d6322
Brandon Chinn <brandon@leapyear.io>
2019-09-27 14:11:48 -0700

    Add tests for PageLinks

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,10 @@
 import Test.Tasty (defaultMain, testGroup)
 
 import qualified Endpoint
+import qualified PageLinks
 
 main :: IO ()
 main = defaultMain $ testGroup "github-rest"
   [ testGroup "GitHub.REST.Endpoint" Endpoint.tests
+  , testGroup "GitHub.REST.PageLinks" PageLinks.tests
   ]
