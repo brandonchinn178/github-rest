6481b92cbed509f7ff987c6adbb422b669616a17
Brandon Chinn <brandon@leapyear.io>
2019-09-30 15:41:58 -0700

    Add test for .:

--- test/Main.hs
+++ test/Main.hs
@@ -1,12 +1,14 @@
 import Test.Tasty (defaultMain, testGroup)
 
 import qualified Endpoint
+import qualified Helpers
 import qualified MockQuery
 import qualified PageLinks
 
 main :: IO ()
 main = defaultMain $ testGroup "github-rest"
-  [ testGroup "GitHub.REST.Endpoint" Endpoint.tests
+  [ testGroup "GitHub.REST (Helpers)" Helpers.tests
+  , testGroup "GitHub.REST.Endpoint" Endpoint.tests
   , testGroup "GitHub.REST.Monad.Class" MockQuery.tests
   , testGroup "GitHub.REST.PageLinks" PageLinks.tests
   ]
