12b4b8c0dbb678d271904cde368ba4a3a5432c83
Brandon Chinn <brandon@leapyear.io>
2021-06-02 15:09:50 -0700

    Accept args in hlint.sh

--- scripts/hlint.sh
+++ scripts/hlint.sh
@@ -4,7 +4,10 @@
 
 set -eo pipefail
 
-builtin cd "$(dirname "${BASH_SOURCE[0]}")/.."
+ARGS=("$@")
+if [[ "${#ARGS}" == 0 ]]; then
+    ARGS+=(.)
+fi
 
 stack build hlint
-stack exec -- hlint .
+stack exec -- hlint "${ARGS[@]}"
