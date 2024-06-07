8afd18c56651d2a3d168d7ac3df77ca5c870223a
Brandon Chinn <brandonchinn178@gmail.com>
2024-06-06 22:35:59 -0700

    Rewrite integration tests as unit tests

--- test/Query.hs
+++ test/Query.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -6,11 +5,11 @@ module Query (tests) where
 
 import Data.Aeson (Value)
 import Data.ByteString.Lazy (ByteString)
-import qualified Data.ByteString.Lazy.Char8 as Char8
 import qualified Data.Text.Lazy.Encoding as Text
 import Network.HTTP.Types (status404)
 import Test.Tasty (TestName, TestTree)
 import Test.Tasty.Golden (goldenVsString)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 import GitHub.REST
 
@@ -19,29 +18,28 @@ tests =
   [ goldens "Query Gist #1" "gists-gist_id-sha.golden" $ do
       gist <- queryGitHub $ getGist 1 "8afc134bf14f8f56ed2e7234128490d9946e8c16"
       return $ Text.encodeUtf8 $ gist .: "files" .: "gistfile1.txt" .: "content"
-  , goldens "Query non-existent Gist commit" "gists-gist_id-sha-422.golden" $ do
-      showResult . githubTry . queryGitHub $
-        getGist 1 "d00770679ba293a327156b9e7031c47c6d269157"
-  , goldens "Query non-existent Gist" "gists-gist_id-sha-404.golden" $ do
-      showResult . githubTry' status404 . queryGitHub $
-        getGist 0 "d00770679ba293a327156b9e7031c47c6d269157"
+  , testCase "Query non-existent Gist commit" $ do
+      (Left e :: Either Value Value) <-
+        runGitHubT ghSettings . githubTry . queryGitHub $
+          getGist 1 "d00770679ba293a327156b9e7031c47c6d269157"
+      e .: "message" @?= ("No commit found for SHA: d00770679ba293a327156b9e7031c47c6d269157" :: String)
+  , testCase "Query non-existent Gist" $ do
+      (Left e :: Either Value Value) <-
+        runGitHubT ghSettings . githubTry' status404 . queryGitHub $
+          getGist 0 "d00770679ba293a327156b9e7031c47c6d269157"
+      e .: "message" @?= ("Not Found" :: String)
   ]
 
 goldens :: TestName -> String -> GitHubT IO ByteString -> TestTree
-goldens name fp action = goldenVsString name ("test/goldens/" ++ fp) $ runGitHubT state action
-  where
-    state =
-      GitHubSettings
-        { token = Nothing
-        , userAgent = "github-rest"
-        , apiVersion = ""
-        }
+goldens name fp action = goldenVsString name ("test/goldens/" ++ fp) $ runGitHubT ghSettings action
 
-showResult :: (Monad m) => m (Either Value Value) -> m ByteString
-showResult m =
-  m >>= \case
-    Right v -> error $ "Got back invalid result: " ++ show v
-    Left e -> return $ Char8.pack $ show e
+ghSettings :: GitHubSettings
+ghSettings =
+  GitHubSettings
+    { token = Nothing
+    , userAgent = "github-rest"
+    , apiVersion = ""
+    }
 
 getGist :: Int -> String -> GHEndpoint
 getGist gistId gistSha =
